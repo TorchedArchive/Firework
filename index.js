6c05edc8d9b68dd72c3d2d03b42ddc158f267821
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-07-22 13:13:50 -0400

    objDeepMerge for Client#addGenericValues function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
 	// Command: require('./src/BaseCommand'),
 	Collection: require('./src/Collection'),
 	Log: require('./src/Log'),
+	utils: require('./src/Utils'),
 	version: require('./package.json').version,
 	erisVersion: require('./package.json').dependencies.eris.slice(1)
 };
