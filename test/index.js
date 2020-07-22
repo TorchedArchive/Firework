c61c578ab7493e65f7704f27b3e2b7c5f71ef7f7
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-07-21 23:20:16 -0400

    update example

--- test/index.js
+++ test/index.js
@@ -1,5 +1,14 @@
 const Discordia = require('../index');
-const bot = new Discordia.Client(require('./config.json'), {env: true});
+const bot = new Discordia.Client(require('./config.json'), {env: true, clientValues: {version: require('./package.json').version} });
+
+bot.on('ready', () => {
+	bot.setGenericEmbed({
+		color: 0x38B1D0,
+		footer: {text: `Version ${bot.version}`},
+		timestamp: new Date()
+	});
+	bot.log.log(`${bot.user.username} is online! Version ${bot.version}`);
+});
 
 bot.on('messageCreate', (msg) => {
 	if (msg.author.bot) return;
@@ -11,12 +20,42 @@ bot.on('messageCreate', (msg) => {
 	for (const pref of prefixes) if (mssg.startsWith(pref)) prefix = pref;
 
 	if (!prefix) return;
+	// TODO: change to function (Client#setGenericValue) for example.
+	// eslint-disable-next-line camelcase
+	bot._options.genericEmbed.footer.icon_url = msg.author.avatarURL;
 
 	const args = msg.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 	try {
-		if (!['ping'].includes(command)) return;
-		msg.channel.createMessage({embed: {generic: true} });
+		switch (command) {
+			case 'ping':
+				msg.channel.createMessage('Pong...?').then(m => {
+					m.edit({
+						content: 'Pong with no generics',
+						embed: {
+							color: 0x47aef0,
+							description: `📬 Latency: \`${bot.shards.get(0).latency}ms\``
+						}
+					});
+					setTimeout(() => {
+						m.edit({
+							content: 'Pong with generics',
+							embed: {
+								generic: true,
+								color: 0x47aef0,
+								description: `📬 Latency: \`${bot.shards.get(0).latency}ms\``
+							}
+						});
+					}, 5000);
+				});
+				break;
+
+			case 'wip':
+				msg.channel.createMessage({embed: {generic: true, description: 'Hi, this is a WIP command as you can tell.'} });
+				break;
+
+			// No default
+		}
 	} catch (err) {
 		bot.log.error(err);
 	}
