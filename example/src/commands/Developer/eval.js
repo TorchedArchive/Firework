de6d94536453f7fd505d58f875d2a94716b5ab10
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-07-25 22:05:35 -0400

    lint

--- example/src/commands/Developer/eval.js
+++ example/src/commands/Developer/eval.js
@@ -6,37 +6,34 @@ class EvalCommand extends Discordia.Command {
 	}
 
 	run(msg, args) {
-		if (!args[0]) return msg.channel.createMessage("So.. where is the code?")
-        try {
-            let code = args.join(" ")
-            var evaled = eval(code.replace(new RegExp('--silent', 'g'), ''));
-            if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
-            if (code.includes("--silent")) {
-                msg.channel.createMessage(this.clean(evaled))
-            } else {
-                msg.channel.createMessage({
-                    embed: {
-                    	generic: true,
-                        description: `\`\`\`js\n${this.clean(evaled)}\n\`\`\``
-                    }
-                })
-            }
-        } catch (err) {
-        	msg.channel.createMessage({
-                embed: {
-                 	generic: true,
-                    description: `\`\`\`js\n${this.clean(err.stack)}\n\`\`\``
-                }
-            })
-        }
+		if (!args[0]) return msg.channel.createMessage('So.. where is the code?');
+		try {
+			const code = args.join(' ')
+			let evaled = eval(code.replace(new RegExp('--silent', 'g'), ''));
+			if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
+			if (code.includes('--silent')) {
+				msg.channel.createMessage(this.clean(evaled));
+			} else {
+				msg.channel.createMessage({
+					embed: {
+						generic: true,
+						description: `\`\`\`js\n${this.clean(evaled)}\n\`\`\``
+					}
+				});
+			}
+		} catch (err) {
+			msg.channel.createMessage({
+				embed: {
+					generic: true,
+					description: `\`\`\`js\n${this.clean(err.stack)}\n\`\`\``
+				}
+			});
+		}
 	}
 
 	clean(text) {
-	    if (typeof(text) === "string") {
-	    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-	    } else {
-	        return text;
-	    }
+		if (typeof(text) === "string") return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+		else return text;
 	}
 }
 
