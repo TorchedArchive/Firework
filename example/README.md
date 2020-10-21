b2cd92cf4fef89f38ad0882373375b8b644fdc65
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-10-20 23:50:10 -0400

    delete old

--- example/README.md
+++
@@ -1,3 +0,0 @@
-# discordia-example
-An example bot for Discordia that uses most (if not all) of its extra features.  
-Feel free to take this code and use it for any bot.
--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	Client: require('./src/Client'),
-	Command: require('./src/BaseCommand'),
-	Collection: require('./src/Collection'),
-	Log: require('./src/Log'),
-	utils: require('./src/Utils'),
-	version: require('./package.json').version,
-	erisVersion: require('./package.json').dependencies.eris.slice(1)
-};
