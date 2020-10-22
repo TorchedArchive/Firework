ce0816deddd1ae3a9d617d8f802c08d460275bff
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-10-21 21:33:26 -0400

    update with new methods

--- example/events/messageCreate.js
+++ example/events/messageCreate.js
@@ -7,17 +7,17 @@ class MessageCreateEvent extends Firework.Event {
 
 	run(msg) {
 		const prefix = '!';
+		if (!msg.content.startsWith(prefix)) return;
 		if (msg.author.bot) return;
-		if (!msg.content.startsWith(prefix)) return;		
 
 		const args = msg.content.slice(prefix.length).trim().split(' ');
-		const command = args.shift().toLowerCase();
+		const command = args.shift().toLowerCase();		
 
 		try {
-			if (!this.bot.commands.has(command)) return;
-			const cmd = this.bot.commands.get(command) || this.bot.commands.get(this.bot.aliases.get(command));
+			const cmd = bot.getCommand(msg.content) // works with the name and aliases as well
+			if (!cmd) return;
 
-			cmd.run({msg, args});
+			cmd.run({ msg, args });
 		} catch (err) {
 			this.bot.logger.error(err);
 		}
